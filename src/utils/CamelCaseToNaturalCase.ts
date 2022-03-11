4cc6cf8281b5aeb5f794d6f9246e15d364b2473a
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 14:11:55 +0530

    Removed useless files

--- src/utils/CamelCaseToNaturalCase.ts
+++
@@ -1,4 +0,0 @@
-import { regexThatTargetsAllUppercaseInString } from './regex';
-
-export const CamelCaseToNaturalCase = (stringToConvert: string) =>
-	stringToConvert.split(regexThatTargetsAllUppercaseInString).join(' ');
