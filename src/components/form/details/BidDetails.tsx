91c3219cb535cc4ee2c93a3dad9f1c66024438ec
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 22:36:40 +0530

    Create BidDetails.tsx
    Common bid details component

---
+++ src/components/form/details/BidDetails.tsx
@@ -0,0 +1,45 @@
+import { Divider, Grid, Typography } from '@mui/material';
+import { Rupee } from 'assets';
+import { useAppDispatch, useAppSelector } from 'store';
+import { appDataInReduxStore } from 'store/app/appSlice';
+
+const BidDetails = () => {
+	const dispatch = useAppDispatch();
+
+	const { bidDetails } = useAppSelector(appDataInReduxStore);
+	const { mobile, name, price, remarks } = bidDetails;
+	return (
+		<div>
+			{' '}
+			<Grid container>
+				<Grid item xs={12}>
+					<Typography variant='caption' color={'GrayText'}>
+						BID DETAILS
+					</Typography>
+				</Grid>
+			</Grid>
+			<Grid container>
+				<Grid item xs={8}>
+					<Typography variant='subtitle1'>+91-{mobile}</Typography>
+					<Typography variant='subtitle1'>{name}</Typography>
+					{remarks && <Typography variant='subtitle1'>{remarks}</Typography>}
+				</Grid>
+				<Grid item xs={4}>
+					<Grid item xs={12} container justifyContent={'center'} alignItems='center'>
+						<Typography variant='h5'>
+							<Rupee /> {price}
+						</Typography>
+					</Grid>
+					<Grid item xs={12} container justifyContent={'center'}>
+						<Typography variant='caption' color={'GrayText'}>
+							Fixed Price{' '}
+						</Typography>
+					</Grid>
+				</Grid>
+			</Grid>
+			<Divider sx={{ my: 5 }} />
+		</div>
+	);
+};
+
+export default BidDetails;
