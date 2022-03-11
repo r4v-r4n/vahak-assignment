71dac9b0c39a9f36dcaaa5cf591f03afe7bab020
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 14:23:56 +0530

    Form one done

--- src/components/form/forms/StepOne.tsx
+++ src/components/form/forms/StepOne.tsx
@@ -1,5 +1,87 @@
+import { Button, Grid } from '@mui/material';
+import { FormikControl } from 'common';
+import { Form, Formik } from 'formik';
+import { useAppDispatch } from 'store';
+import { journeyDetailsReducer } from 'store/app/appSlice';
+import * as yup from 'yup';
+
+type Values = {
+	sourceLocation: string;
+	destination: string;
+	travellers: string;
+	carType: string;
+};
+
 const StepOne = () => {
-	return <div>StepOne</div>;
+	const dispatch = useAppDispatch();
+	const carTypeOptions = [
+		{ key: 'hatchBack', value: 'HatchBack' },
+		{ key: 'sedan', value: 'Sedan' },
+		{ key: 'suv', value: 'SUV' },
+	];
+
+	const initialValues = {
+		sourceLocation: '',
+		destination: '',
+		travellers: '',
+		carType: '',
+	};
+
+	const validationSchema = yup.object({
+		sourceLocation: yup.string().required('Source location is required'),
+		destination: yup.string().required('Destination is required'),
+		carType: yup.string().required('Car Type is required'),
+		travellers: yup.number().required('Travellers is required'),
+	});
+	const onSubmit = (values: Values) => {
+		console.log(values);
+		dispatch(journeyDetailsReducer(values));
+	};
+
+	return (
+		<>
+			<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+				{() => (
+					<Form>
+						<Grid container spacing={2}>
+							<Grid item xs={12} sm={6}>
+								<FormikControl
+									control='muiInput'
+									name='sourceLocation'
+									label='Source Location'
+									required
+								/>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<FormikControl control='muiInput' name='destination' label='Destination' required />
+							</Grid>
+
+							<Grid item xs={12}>
+								<FormikControl
+									control='muiSelect'
+									name='carType'
+									label='Enter Car type'
+									options={carTypeOptions}
+									required
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<FormikControl
+									control='muiInput'
+									name='travellers'
+									label='Number of Travellers'
+									type='number'
+								/>
+							</Grid>
+						</Grid>
+						<Button variant='contained' color='primary' type='submit' fullWidth>
+							Enter Bid Details
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</>
+	);
 };
 
 export default StepOne;
--- src/store/app/appSlice.ts
+++ src/store/app/appSlice.ts
@@ -4,11 +4,18 @@ import { RootState } from '..';
 type InitialState = {
 	status: 'idle' | 'loading' | 'failed';
 	step: string;
+	journeyDetails: {
+		sourceLocation: string;
+		destination: string;
+		travellers: string;
+		carType: string;
+	};
 };
 
 const initialState: InitialState = {
 	status: 'idle',
 	step: 'Place your bid (1/4 Step)',
+	journeyDetails: { sourceLocation: '', destination: '', travellers: '', carType: '' },
 };
 
 export const appSlice = createSlice({
@@ -16,14 +23,17 @@ export const appSlice = createSlice({
 	initialState,
 	// The `reducers` field lets us define reducers and generate associated actions
 	reducers: {
-		setStep: (state, action) => {
+		stepReducer: (state, action) => {
 			state.step = action.payload;
+		},
+		journeyDetailsReducer: (state, action) => {
+			state.journeyDetails = action.payload;
 		},
 	},
 });
 
 // exporting reducer so that we can dispatch this in desired page(s) which then will trigger associated action with the reducer
-export const { setStep } = appSlice.actions;
+export const { stepReducer, journeyDetailsReducer } = appSlice.actions;
 
 // exporting the app data object for consumption by page(s)
 export const appDataInReduxStore = (state: RootState) => state.app;
