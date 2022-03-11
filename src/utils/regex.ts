ddf6343ec0310d9e1a74db9d526139b4925752c5
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 13:59:39 +0530

    Update regex.ts
    Removed useless regex

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,5 +1,1 @@
-export const emailRegex = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
-export const mobileRegex = /^\d{10}$/;
-
 export const regexThatTargetsAllUppercaseInString = /(?=[A-Z])/;
