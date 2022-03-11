4f6350482325e236ea3265aac7cf1f51f4680fb1
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 22:47:14 +0530

    Refactored and formated code

--- src/components/form/details/JourneyDetails.tsx
+++ src/components/form/details/JourneyDetails.tsx
@@ -1,27 +1,20 @@
-import { Box, Button, Divider, Grid, InputAdornment, TextField, Typography } from '@mui/material';
-import { Edit, Rupee } from 'assets';
-import { FormikControl } from 'common';
-import { Form, Formik } from 'formik';
-import { useState } from 'react';
-import { useAppDispatch, useAppSelector } from 'store';
-import { appDataInReduxStore, incrementStep, bidDetailsReducer } from 'store/app/appSlice';
-import * as yup from 'yup';
+import { Button, Divider, Grid, Typography } from '@mui/material';
+import { Edit } from 'assets';
+import { useAppSelector } from 'store';
+import { appDataInReduxStore } from 'store/app/appSlice';
 
 const JourneyDetails = () => {
-	const dispatch = useAppDispatch();
-
 	const { journeyDetails } = useAppSelector(appDataInReduxStore);
 	const { source, destination, travellers, carType } = journeyDetails;
 	return (
-		<div>
-			{' '}
+		<>
 			<Grid container spacing={3}>
-				<Grid item xs={8}>
+				<Grid item xs={9}>
 					<Typography variant='caption' color={'GrayText'}>
 						JOURNEY DETAILS
 					</Typography>
 				</Grid>
-				<Grid item xs={3}>
+				<Grid item xs={2}>
 					<Button startIcon={<Edit />}>Edit</Button>
 				</Grid>
 			</Grid>
@@ -32,7 +25,7 @@ const JourneyDetails = () => {
 				{travellers} Persons, {carType}
 			</Typography>
 			<Divider sx={{ my: 5 }} />
-		</div>
+		</>
 	);
 };
 
