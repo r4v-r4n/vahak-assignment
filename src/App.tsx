9addafb9f7c2d4c976f4627112ae27f949f8d901
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 13:25:42 +0530

    Imported all components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Banner from 'components/banner/Banner';
+import Navigation from 'components/Navigation';
+import Form from 'components/form/Form';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Navigation />
+			<Banner />
+			<Form />
+		</>
+	);
 }
 
 export default App;
