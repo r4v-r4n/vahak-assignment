87d1a2baa0fd7651841e8a4f12f1c2fda751e42d
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 22:47:08 +0530

    Barebones setup for step 4

--- src/components/form/forms/StepFour.tsx
+++ src/components/form/forms/StepFour.tsx
@@ -1,5 +1,19 @@
+import { Box, Button } from '@mui/material';
+import BidDetails from '../details/BidDetails';
+import JourneyDetails from '../details/JourneyDetails';
+
 const StepFour = () => {
-	return <div>StepFour</div>;
+	return (
+		<>
+			<JourneyDetails />
+			<BidDetails />
+			<Box mx={3}>
+				<Button variant='contained' color='primary' type='submit' fullWidth>
+					Submit bid
+				</Button>
+			</Box>
+		</>
+	);
 };
 
 export default StepFour;
