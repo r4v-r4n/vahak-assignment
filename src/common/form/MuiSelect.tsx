00a16e36133fd5b7dd56fe32d8193277ac5fc86a
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 14:11:40 +0530

    Update MuiSelect.tsx

--- src/common/form/MuiSelect.tsx
+++ src/common/form/MuiSelect.tsx
@@ -1,29 +1,28 @@
 import { Box, MenuItem, TextField } from '@mui/material';
 import { Field, FieldProps } from 'formik';
-import { CamelCaseToNaturalCase } from 'utils/CamelCaseToNaturalCase';
 
 type MuiSelectProps = {
-
 	name: string;
+	label: string;
 	disabled?: boolean;
 	options?: Array<{ key: string; value: string }>;
 };
 
 function MuiSelect(props: MuiSelectProps) {
-	const {   name, options, ...rest } = props;
+	const { name, label, options, ...rest } = props;
 
 	return (
 		<div className='form-control'>
 			<Field name={name}>
 				{({ field, form }: FieldProps) => (
-					<Box my={3}>
+					<Box my={1}>
 						<TextField
 							select
 							fullWidth
 							size='small'
-							variant='filled'
+							variant='outlined'
 							id={name}
-							label={CamelCaseToNaturalCase(name)}
+							label={label}
 							{...rest}
 							{...field}
 							error={form.errors[name] && form.touched[name] ? true : false}
