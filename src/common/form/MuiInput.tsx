de3182853f781a3e61e59b27ba07fd16f8f65e8e
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 14:11:37 +0530

    Update MuiInput.tsx

--- src/common/form/MuiInput.tsx
+++ src/common/form/MuiInput.tsx
@@ -1,25 +1,26 @@
 import { Box, TextField } from '@mui/material';
 import { Field, FieldProps } from 'formik';
-import { CamelCaseToNaturalCase } from 'utils/CamelCaseToNaturalCase';
 
 type MuiInputProps = {
 	name: string;
 	inputProps?: any;
+	label: string;
 	disabled?: boolean;
 };
 
 const MuiInput = (props: MuiInputProps) => {
-	const { name, ...rest } = props;
+	const { name, label, ...rest } = props;
+
 	return (
 		<Field name={name}>
 			{({ field, form }: FieldProps) => (
-				<Box my={3}>
+				<Box my={1}>
 					<TextField
 						id={name}
-						variant='filled'
+						variant='outlined'
 						size='small'
 						fullWidth
-						label={CamelCaseToNaturalCase(name)}
+						label={label}
 						error={form.errors[name] && form.touched[name] ? true : false}
 						helperText={form.errors[name] && form.touched[name] ? form.errors[name] : null}
 						{...rest}
