82fceb5a107bc9cf75daefab73afcc9d31a1aefe
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 14:55:19 +0530

    Update Form.tsx
    Conditionally rendering form component

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,4 +1,6 @@
 import { Container } from '@mui/material';
+import { useAppSelector } from 'store';
+import { appDataInReduxStore } from 'store/app/appSlice';
 import StepFour from './forms/StepFour';
 import StepOne from './forms/StepOne';
 import StepThree from './forms/StepThree';
@@ -6,15 +8,27 @@ import StepTwoPartOne from './forms/StepTwoPartOne';
 import StepTwoPartTwo from './forms/StepTwoPartTwo';
 
 const Form = () => {
-	return (
-		<Container maxWidth='sm'>
-			<StepOne />
-			<StepTwoPartOne />
-			<StepTwoPartTwo />
-			<StepThree />
-			<StepFour />
-		</Container>
-	);
+	const { activeStepNumber } = useAppSelector(appDataInReduxStore);
+
+	const stepperFunction = (key: number) => {
+		switch (key) {
+			case 1:
+				return <StepTwoPartOne />;
+
+			case 2:
+				return <StepTwoPartTwo />;
+
+			case 3:
+				return <StepThree />;
+
+			case 4:
+				return <StepFour />;
+
+			default:
+				return <StepOne />;
+		}
+	};
+	return <Container maxWidth='sm'>{stepperFunction(activeStepNumber)}</Container>;
 };
 
 export default Form;
