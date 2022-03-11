23acda83cc6bf670f74d40b5e1beb881517b9963
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-11 14:55:28 +0530

    Update StepOne.tsx

--- src/components/form/forms/StepOne.tsx
+++ src/components/form/forms/StepOne.tsx
@@ -1,8 +1,8 @@
-import { Button, Grid } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import { FormikControl } from 'common';
 import { Form, Formik } from 'formik';
 import { useAppDispatch } from 'store';
-import { journeyDetailsReducer } from 'store/app/appSlice';
+import { incrementStep, journeyDetailsReducer } from 'store/app/appSlice';
 import * as yup from 'yup';
 
 type Values = {
@@ -31,38 +31,32 @@ const StepOne = () => {
 		sourceLocation: yup.string().required('Source location is required'),
 		destination: yup.string().required('Destination is required'),
 		carType: yup.string().required('Car Type is required'),
-		travellers: yup.number().required('Travellers is required'),
+		travellers: yup.number().typeError('Must be a number'),
 	});
 	const onSubmit = (values: Values) => {
-		console.log(values);
 		dispatch(journeyDetailsReducer(values));
+		dispatch(incrementStep());
 	};
 
 	return (
-		<>
+		<Box mt={4}>
 			<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
 				{() => (
 					<Form>
 						<Grid container spacing={2}>
 							<Grid item xs={12} sm={6}>
-								<FormikControl
-									control='muiInput'
-									name='sourceLocation'
-									label='Source Location'
-									required
-								/>
+								<FormikControl control='muiInput' name='sourceLocation' label='Source Location *' />
 							</Grid>
 							<Grid item xs={12} sm={6}>
-								<FormikControl control='muiInput' name='destination' label='Destination' required />
+								<FormikControl control='muiInput' name='destination' label='Destination *' />
 							</Grid>
 
 							<Grid item xs={12}>
 								<FormikControl
 									control='muiSelect'
 									name='carType'
-									label='Enter Car type'
+									label='Enter Car type *'
 									options={carTypeOptions}
-									required
 								/>
 							</Grid>
 							<Grid item xs={12}>
@@ -70,17 +64,19 @@ const StepOne = () => {
 									control='muiInput'
 									name='travellers'
 									label='Number of Travellers'
-									type='number'
+									inputProps={{ maxLength: 10 }}
 								/>
 							</Grid>
 						</Grid>
-						<Button variant='contained' color='primary' type='submit' fullWidth>
-							Enter Bid Details
-						</Button>
+						<Box my={3} mx={2}>
+							<Button variant='contained' color='primary' type='submit' fullWidth>
+								Enter Bid Details
+							</Button>
+						</Box>
 					</Form>
 				)}
 			</Formik>
-		</>
+		</Box>
 	);
 };
 
