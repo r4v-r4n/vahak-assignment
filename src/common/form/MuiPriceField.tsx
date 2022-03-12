c3fc280973c09680591711c9196f24d6e3aadf49
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-12 20:51:06 +0530

    Update MuiPriceField.tsx

--- src/common/form/MuiPriceField.tsx
+++ src/common/form/MuiPriceField.tsx
@@ -1,26 +1,25 @@
 import { Box, InputAdornment, TextField } from '@mui/material';
 import { Rupee } from 'assets';
 import { Field, FieldProps } from 'formik';
+import useFormatter from 'hooks/useFormatter';
 
 type MuiPriceFieldProps = {
 	name: string;
 	label?: string;
 	disabled?: boolean;
-	value?: string;
 };
 
 const MuiPriceField = (props: MuiPriceFieldProps) => {
-	const { name, label } = props;
-
+	const { formatPrice } = useFormatter();
+	const { name, label, ...rest } = props;
 	return (
 		<Field name={name}>
 			{({ field, form }: FieldProps) => {
-				const { value, ...rest } = field;
-
+				console.log('🚀 ~ file: MuiPriceField.tsx ~ line 44 ~ MuiPriceField ~ field', field.value);
 				return (
-					<Box my={1} display='flex' justifyContent={'center'}>
+					<Box my={1} display='flex' justifyContent='center'>
 						<TextField
-							placeholder='0,00,000'
+							placeholder='0'
 							InputProps={{
 								startAdornment: (
 									<InputAdornment position='start' sx={{ fontSize: '3rem' }}>
@@ -30,15 +29,15 @@ const MuiPriceField = (props: MuiPriceFieldProps) => {
 								disableUnderline: true,
 							}}
 							inputProps={{
-								maxLength: 7,
-								style: { fontSize: 50, width: 250 },
+								maxLength: 6,
+								style: { fontSize: 50, width: '220px' },
 							}}
 							variant='standard'
 							label={label}
 							error={form.errors[name] && form.touched[name] ? true : false}
 							helperText={form.errors[name] && form.touched[name] ? form.errors[name] : null}
-							value={field.value} //TODO: update this to use comma in between
 							{...rest}
+							{...field}
 						/>
 					</Box>
 				);
