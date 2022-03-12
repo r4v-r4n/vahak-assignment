baae27eb86f4961c78b11283c0eb6e37623c645a
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-12 15:54:58 +0530

    Update regex.ts

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,4 +1,4 @@
 export const regexForSuv = /^([1-6])/;
 export const regexForOtherCarTypes = /^([1-4])/;
 export const mobileRegex = /^\d{10}$/;
-export const nameRegex= /^[a-zA-Z ]{2,30}$/;
+export const nameRegex = /^[a-zA-Z ]/;
