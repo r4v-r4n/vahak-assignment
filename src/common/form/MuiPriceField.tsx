d523b0413c7adda9920e1a508b8bb8dd246b4947
errajeevranjan <rjeevrnjn17@gmail.com>
2022-03-12 17:49:13 +0530

    Update MuiPriceField.tsx

--- src/common/form/MuiPriceField.tsx
+++ src/common/form/MuiPriceField.tsx
@@ -6,37 +6,43 @@ type MuiPriceFieldProps = {
 	name: string;
 	label?: string;
 	disabled?: boolean;
-	variant?: 'outlined' | 'filled' | 'standard';
+	value?: string;
 };
 
 const MuiPriceField = (props: MuiPriceFieldProps) => {
-	const { name, variant, label, ...rest } = props;
+	const { name, label } = props;
 
 	return (
 		<Field name={name}>
-			{({ field, form }: FieldProps) => (
-				<Box my={1}>
-					<TextField
-						placeholder='0'
-						InputProps={{
-							startAdornment: (
-								<InputAdornment position='start'>
-									<Rupee />
-								</InputAdornment>
-							),
-							disableUnderline: true,
-						}}
-						inputProps={{ maxLength: 10 }}
-						variant='standard'
-						fullWidth
-						label={label}
-						error={form.errors[name] && form.touched[name] ? true : false}
-						helperText={form.errors[name] && form.touched[name] ? form.errors[name] : null}
-						{...rest}
-						{...field}
-					/>
-				</Box>
-			)}
+			{({ field, form }: FieldProps) => {
+				const { value, ...rest } = field;
+
+				return (
+					<Box my={1} display='flex' justifyContent={'center'}>
+						<TextField
+							placeholder='0,00,000'
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position='start' sx={{ fontSize: '3rem' }}>
+										<Rupee />
+									</InputAdornment>
+								),
+								disableUnderline: true,
+							}}
+							inputProps={{
+								maxLength: 7,
+								style: { fontSize: 50, width: 250 },
+							}}
+							variant='standard'
+							label={label}
+							error={form.errors[name] && form.touched[name] ? true : false}
+							helperText={form.errors[name] && form.touched[name] ? form.errors[name] : null}
+							value={field.value} //TODO: update this to use comma in between
+							{...rest}
+						/>
+					</Box>
+				);
+			}}
 		</Field>
 	);
 };
